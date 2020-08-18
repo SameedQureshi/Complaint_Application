63489fa2eb92b297ef6b35d3c439c4f75a0e8300
SameedQureshi <48343490+SameedQureshi@users.noreply.github.com>
2020-08-18 11:59:58 +0500

    Add files via upload

---
+++ Complaint_Application/Default.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Complaint_Application
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CallingApi();
+        }
+
+        #region Function that call the api
+        public void CallingApi()
+        {
+            HttpWebResponse response;
+            string result = string.Empty;
+            string email = "test";
+            string password = "test123";
+            if (Request_Login(out response, email, password))
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    result = reader.ReadToEnd();
+                }
+                response.Close();
+            }
+
+            int userId = 1;
+            string complaint = "Hello World!..";
+            if (FetchComplaints(out response, complaint, userId))
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    result = reader.ReadToEnd();
+                }
+                response.Close();
+            }
+
+        }
+        #endregion
+
+        #region Region_login function use for Login.ashx service  
+        //if login credentials are correct then it will return true other wise false 
+        private bool Request_Login(out HttpWebResponse response, string email, string password)
+        {
+            response = null;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:1179//Services/Login.ashx");
+
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+                request.Accept = "/";
+                request.Headers.Set(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+                request.Headers.Set(HttpRequestHeader.AcceptLanguage, "en-US,en;q=0.8");
+
+                request.Method = "POST";
+                request.ServicePoint.Expect100Continue = false;
+
+                string body = @"email=" + email + "&password=" + password;
+                byte[] postBytes = System.Text.Encoding.UTF8.GetBytes(body);
+                request.ContentLength = postBytes.Length;
+                Stream stream = request.GetRequestStream();
+                stream.Write(postBytes, 0, postBytes.Length);
+                stream.Close();
+
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                if (e.Status == WebExceptionStatus.ProtocolError) response = (HttpWebResponse)e.Response;
+                else return false;
+            }
+            catch (Exception)
+            {
+                if (response != null) response.Close();
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region Fetch complaint for log a complaint and fetch it.
+        private bool FetchComplaints(out HttpWebResponse response, string complaint, int userId)
+        {
+            response = null;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:1179/Services/Complaint.ashx");
+
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+                request.Accept = "/";
+                request.Headers.Set(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+                request.Headers.Set(HttpRequestHeader.AcceptLanguage, "en-US,en;q=0.8");
+
+                request.Method = "POST";
+                request.ServicePoint.Expect100Continue = false;
+
+                string body = @"Complaint=" + complaint + "&UserId=" + userId;
+                byte[] postBytes = System.Text.Encoding.UTF8.GetBytes(body);
+                request.ContentLength = postBytes.Length;
+                Stream stream = request.GetRequestStream();
+                stream.Write(postBytes, 0, postBytes.Length);
+                stream.Close();
+
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                if (e.Status == WebExceptionStatus.ProtocolError) response = (HttpWebResponse)e.Response;
+                else return false;
+            }
+            catch (Exception)
+            {
+                if (response != null) response.Close();
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ Complaint_Application/Global.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Complaint_Application
+{
+    public class Global
+    {
+        public static int CountRows(string query, string connString)
+        {
+            int Count = 0;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        // define command to be stored procedure
+                        cmd.CommandType = CommandType.Text;
+                        // open connection, execute command, close connection
+                        conn.Open();
+                        Count = (int)cmd.ExecuteScalar();
+                        conn.Close();
+                    }
+                }
+                return Count;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return Count;
+        }
+        public static DataTable GetDataTable(string query, string connString)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        SqlDataAdapter da = new SqlDataAdapter();
+                        da.SelectCommand = cmd;
+                        da.Fill(dt);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return dt;
+        }
+        public static void InsertDeleteUpdate(string query, string constring)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(constring))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.ExecuteNonQuery();
+                        cmd.Dispose();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("InsUpDel: " + Environment.NewLine + query + Environment.NewLine + ex);
+            }
+        }
+    }
+}
